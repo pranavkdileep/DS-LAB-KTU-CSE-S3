c4cb2621a58c5c2390acb587ee46fb107c4362ca
Tw0side <twoside2004@gmail.com>
2023-11-24 02:39:50 -0500

    infixtoposfix

--- infixToPostfix.c
+++ infixToPostfix.c
@@ -1,86 +1,64 @@
 #include<stdio.h>
 #include <ctype.h>
 
-char a[20],p[20];
-char s[20];
-int top=-1;
+char a[20], p[20], s[20];
+int top = -1;
 
-int precedence(char op)
-{
-    switch(op)
-    {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-            return 2;
-        case '^':
-            return 3;
-    }
+int precedence(char op) {
+    if(op == '+' || op == '-')
+        return 1;
+    if(op == '*' || op == '/')
+        return 2;
+    if(op == '^')
+        return 3;
+    return 0;
 }
-void infixToPostfix()
-{
-    int i =0,j=0;
-    char x,y;
-    while(a[i] != '\0')
-    {
-        if(isdigit(a[i]))
-        {
-            p[j] = a[i];
-            j++;
-        }
-        else if (a[i] == '(')
-        {
-            top++;
-            s[top] = a[i];
-        }
-        else if (a[i] == ')')
-        {
+
+int isRightAssociative(char op) {
+    if(op == '^')
+        return 1;
+    return 0;
+}
+
+void push(char x) {
+    s[++top] = x;
+}
+
+char pop() {
+    if(top == -1)
+        return -1;
+    else
+        return s[top--];
+}
+
+void infixToPostfix() {
+    int i = 0, j = 0;
+    char x;
+    while((x = a[i++]) != '\0') {
+        if(isalnum(x))
+            p[j++] = x;
+        else if(x == '(')
+            push(x);
+        else if(x == ')') {
             while(s[top] != '(')
-            {
-               p[j] = s[top];
-               j++;
-               top--; 
-            }
+                p[j++] = pop();
             top--;
         }
-        else
-        {
-            x=a[i];
-            y=s[top];
-            if(top == -1 || s[top] == '(' || precedence(x)>precedence(y))
-            {
-                top++;
-                s[top] = x;
-            }
-            else
-            {
-                while(top != -1 && y != '(' && (precedence(x)<=precedence(s[top])))
-                {
-                    p[j] = s[top];
-                    top--;
-                    j++;
-                }
-                top++;
-                s[top] = x;
-            }
+        else {
+            while(top != -1 && (precedence(s[top]) >= precedence(x) && !isRightAssociative(x)))
+                p[j++] = pop();
+            push(x);
         }
-        i++;
     }
     while(top != -1)
-    {
-        p[j] = s[top];
-        j++;
-        top--;
-    }
-    p[j]='\0';
-    printf("%s\n",p);
+        p[j++] = pop();
+    p[j] = '\0';
+    printf("%s\n", p);
 }
-int main()
-{
-printf("enter infix :");
-scanf("%s",&a);
-infixToPostfix();
-return 0;
+
+int main() {
+    printf("Enter infix: ");
+    scanf("%s", a);
+    infixToPostfix();
+    return 0;
 }
